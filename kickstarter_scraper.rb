0b2d9708b175d7459bc1d976259475ee9f6e5860
Shahwar Spall <shevy.spall@gmail.com>
2019-09-02 03:40:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,8 +7,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects={}
  # binding.pry
-end
-
+ 
 kickstarter.css("li.project.grid_4").each do |project|
   title = project.css("h2.bbcard_name strong a").text
   
